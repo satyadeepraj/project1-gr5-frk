cadb88401053cdd460d5452a464284f29a6eb13a
satyadeepraj <satyadeep@gmail.com>
2023-04-19 02:29:22 +0530

    commit changes

--- src/Pages/Training/Training.jsx
+++ src/Pages/Training/Training.jsx
@@ -1,13 +1,90 @@
-import React from 'react'
-import Header from '../Header/Header'
+import React from "react";
+import Carousel from "./Carousel.jsx";
 
-function Training() {
+function Tranning() {
+  const data = [
+    {
+      image:
+        "https://images.pexels.com/photos/116077/pexels-photo-116077.jpeg?auto=compress&cs=tinysrgb&w=600",
+      caption: "Body Building",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/3822864/pexels-photo-3822864.jpeg?auto=compress&cs=tinysrgb&w=600",
+      caption: "Yoga",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      caption: "Strength training",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/1954524/pexels-photo-1954524.jpeg?auto=compress&cs=tinysrgb&w=600",
+      caption: "Cardio Exercise",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/4498151/pexels-photo-4498151.jpeg?auto=compress&cs=tinysrgb&w=600",
+      caption: "Aerobic",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg?auto=compress&cs=tinysrgb&w=600",
+      caption: "Core exercises",
+    },
+  ];
+
+  const captionStyle = {
+    fontSize: "2em",
+    fontWeight: "bold",
+  };
+  const slideNumberStyle = {
+    fontSize: "20px",
+    fontWeight: "bold",
+  };
+  
   return (
-    <div>
-      <Header/> 
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio suscipit, explicabo mollitia ducimus recusandae ipsa quaerat quidem officia omnis quisquam possimus? Nostrum eligendi asperiores autem facere aliquid suscipit nobis libero vero. Sunt dicta est, laborum, dolores similique ipsum, labore facere non inventore dolor a eligendi cum possimus debitis! Eaque placeat incidunt natus, quia eos esse quae delectus eligendi. Quae, dolore?
+    <div className="Tranning">
+      <div style={{ textAlign: "center" }}>
+        <h2 style={{color:"orange" ,fontSize:"5rem"}}>Training Programs</h2>
+        <p>
+       
+        </p>
+        <div
+          style={{
+            padding: "0 20px",
+          }}
+        >
+          <Carousel
+            data={data}
+            time={2000}
+            width="850px"
+            height="500px"
+            captionStyle={captionStyle}
+            radius="10px"
+            slideNumber={true}
+            slideNumberStyle={slideNumberStyle}
+            captionPosition="bottom"
+            automatic={true}
+            dots={true}
+            pauseIconColor="white"
+            pauseIconSize="40px"
+            slideBackgroundColor="darkgrey"
+            slideImageFit="cover"
+            thumbnails={true}
+            thumbnailWidth="100px"
+            style={{
+              textAlign: "center",
+              maxWidth: "850px",
+              maxHeight: "500px",
+              margin: "40px auto",
+            }}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Training
+export default Tranning;
